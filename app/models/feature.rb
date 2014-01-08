1c11912df38587af4db64107964e46a81cc092d8
Pandurang <pandurang.plw@gmail.com>
2014-01-08 13:58:55 +0530

    ui changes

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,21 +4,5 @@ class Feature < ActiveRecord::Base
   validates :name, presence: true
 
   attr_accessible :name, :status
-  #after_save :update_hash
-  
-  #@@features = {}
 
- # 	def self.features
- # 		#@@features = {}
- # 		@@features.present? ? @@features : Feature.all.map{|f| @@features[f.name.to_s.intern] = f.status} 
- # 		@@features
- # 	end
-
-	# def update_hash
-	# 	@@features = {}
- #  	Feature.all.map{|f| @@features[f.name.to_s.intern] = f.status}
- #  	@@features.freeze
- #  	puts @@features.inspect
- #  end
-	
 end
